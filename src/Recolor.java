389d518ff12da7f9657a1d62a238572f6b836a31
szilard <csabika94@yahoo.com>
2022-12-08 20:17:58 +0100

    JAR FILE About
    Recolor javitva, jol mukodik

--- src/Recolor.java
+++ src/Recolor.java
@@ -1,4 +1,5 @@
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
 
 
 public class Recolor extends AlgorithmsMentor {
@@ -6,18 +7,33 @@ public class Recolor extends AlgorithmsMentor {
     @Override
     public void actionPerformed(ActionEvent evt) {
 
-        expandNode();
-        repaint();
-        if (LabirWindow.megtalalt || LabirWindow.nyitottPoz.isEmpty()) {
+        //expandNode();
+        //repaint();
+        if (LabirWindow.nyitottPoz.isEmpty()) {
+            LabirWindow.keresesVege = true;
+            LabirWindow.info.setText("Nincs út a célhoz!");
+        } else {
+            expandNode();
+
+            if (LabirWindow.megtalalt) {
+
+                LabirWindow.keresesVege = true;
+                plotRoute();
+                LabirWindow.ido.stop();
+            }
+        }
+        /*if (LabirWindow.megtalalt  || LabirWindow.nyitottPoz.isEmpty()) {
             LabirWindow.ido.stop();
             LabirWindow.keresesVege = true;
-
+            //expandNode();
             if (LabirWindow.nyitottPoz.isEmpty()) {
                 LabirWindow.info.setText("Nincs út a célhoz!!!");
             } else {
                 plotRoute();
+                //expandNode();
+                //plotRoute();
             }
-        }
+        }*/
         repaint();
     }
 }
